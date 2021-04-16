fb91db00d04fb0205326a0d4725499f8d765911c
petres <peter.reschenhofer@gmail.com>
2021-04-17 01:15:22 +0200

    fix bug, remove package

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
